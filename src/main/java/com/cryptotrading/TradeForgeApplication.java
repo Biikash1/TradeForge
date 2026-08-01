2330de165f6e88ca34a038d3c499807c378c121e
Biikash1 <bm7915911@gmail.com>
2026-08-01 12:40:31 +0530

    Today, set the TradeForge syatem

---
+++ src/main/java/com/cryptotrading/TradeForgeApplication.java
@@ -0,0 +1,13 @@
+package com.cryptotrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeForgeApplication.class, args);
+	}
+
+}
